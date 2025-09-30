d8c9b67df6607c181fda37d0be805e5146c2f8f8
burgessleia1 <burgessleia1@gmail.com>
2025-09-30 10:09:43 -0600

    final

--- scipt.js
+++ scipt.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById
 ('question-container')
 const questionElement = document.getElementById('question')
@@ -6,9 +7,13 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => (
+    currentQuestionIndex++
+    setNextQuestion()
+))
+
 
 function startGame() {
-    console.log('Started')
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -30,15 +35,51 @@ function showQuestion(question) {
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
-        button.addEventListener('click', selectAnswer)
+         button.addEventListener('click', selectAnswer)
         answerButtonsElement.appendChild(button)
+    })
+}      
+       
 
-        
+ function resetState()  {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+ }   
 
 
-function selectAnswer() {}
-
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button=>{
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+    }
 }
+
+function setStatusClass(element, corredt) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')   
+    } else {
+        element.classList.add('wrong')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+
 const questions = [
     {
         question: 'What is 2 + 2?',
@@ -48,3 +89,42 @@ const questions = [
             ]
     }
 ]
+{
+        question: 'What is 2 + 2?',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+            ]
+    }
+{
+        question: 'Who is the best Youtuber?',
+        answers: [
+            { text: 'Web Dev Simplified', correct: true },
+            { text: 'Traversy Media', correct: true },
+            { text: 'Cev Ed', correct: true },
+            { text: 'Fun Fun Function', correct: true },
+            ]
+    }
+ {
+        question: 'Is web development fun?',
+        answers: [
+            { text: 'Kinda', correct: true },
+            { text: 'Yes!!!', correct: true },
+            { text: 'Um no', correct: true },
+            { text: 'IDK', correct: true },
+            ]
+    }
+    {
+        question: 'What is 4 + 2?',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+            ]
+        }
+     {
+        question: 'What is 2 + 2?',
+        answers: [
+            { text: '6', correct: true },
+            { text: '28', correct: false }
+            ]
+    }   
